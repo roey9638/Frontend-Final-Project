9992d32a60c093d5747f63cc9aa4de3089f9a228
roey9638 <roey723@gmail.com>
2022-11-27 15:20:29 +0200

    End of section 21

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -7,7 +7,8 @@ import { history } from "../..";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../Models/user";
 //import { config } from "process";
-import { Photo, Profile } from "../Models/profile";
+import { Photo, Profile, UserActivity } from "../Models/profile";
+import { PaginatedResult } from "../Models/pagination";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -28,6 +29,12 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginatedResult<any>>
+    }
+
     return response;
     //The [AxiosError]. I [Changed] in the the from the [T = unknown] to [T = any]
 }, (error: AxiosError) => { //Everything that is not (200) [Response] will be [error: AxiosError / Rejected]
@@ -102,7 +109,7 @@ const requests = {
 
 const Activities = {
     //This [requests.get('/activities')] will be the [base URL] on (top^^) plus what ever we put after it which wil be the ('/') for example (/activities)
-    list: () => requests.get<Activity[]>('/activities'),
+    list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', { params }).then(responseBody),
     //The [Back ticks (` `)] is so that we could [pass in] the [Id] of a [specific activity]
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
@@ -132,8 +139,8 @@ const Profiles = {
     updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
     listFollowings: (username: string, predicate: string) =>
         requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
-     //listActivities: (username: string, predicate: string) =>
-         //requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
+    listActivities: (username: string, predicate: string) =>
+         requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 
 }
 
--- src/features/activities/dashboard/ActivityDashBoard.tsx
+++ src/features/activities/dashboard/ActivityDashBoard.tsx
@@ -1,43 +1,66 @@
-import React, { useEffect } from "react";
-import { Grid, GridColumn } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Grid, Loader } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { observer } from 'mobx-react-lite';
 import ActivityFilters from "./ActivityFilters";
-
+import { PagingParams } from "../../../app/Models/pagination";
+import InfiniteScroll from 'react-infinite-scroller';
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder";
 
 
 export default observer(function ActivityDashBoard() {
 
     //Here I'm [destructuring] the [activityStore] fron the [useStore()].
     const { activityStore } = useStore();
-    const { loadActivities, activityRegistry } = activityStore
+    const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore
+    const [loadingNext, setLoadingNext] = useState(false);
+
+    function handleGetNext() {
+        setLoadingNext(true);
+        setPagingParams(new PagingParams(pagination!.currentPage + 1))
+        loadActivities().then(() => setLoadingNext(false));
+    }
 
     useEffect(() => {
-       if(activityRegistry.size <= 1)
-       {
-         loadActivities();
-       } 
+        if (activityRegistry.size <= 1) {
+            loadActivities();
+        }
     }, [activityRegistry.size, loadActivities])
 
 
     //What this do - If The [activityStore.loadingInitial] which is a [property] in [ActivityStore.ts]. then will [return/execute] the [<LoadingComponent content='Loading app' />].
-    if (activityStore.loadingInitial) 
-    {
+    if (activityStore.loadingInitial && !loadingNext) {
         return <LoadingComponent content='Loading activities...' />
     }
 
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList />
+                {activityStore.loadingInitial && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder />
+                        <ActivityListItemPlaceholder />
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={handleGetNext}
+                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                        initialLoad={false}
+                    >
+                        <ActivityList />
+                    </InfiniteScroll>
+                )}
+            </Grid.Column>
+            <Grid.Column width='6'>
+                <ActivityFilters />
             </Grid.Column>
 
-            <GridColumn width='6'>
-                <ActivityFilters />
-            </GridColumn>
-
+            <Grid.Column width={10}>
+                <Loader active={loadingNext} />
+            </Grid.Column>
         </Grid>
     )
 })
